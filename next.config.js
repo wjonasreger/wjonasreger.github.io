fbecf1b51c6b695eade32038f09815d845cb2944
wjonasreger <wjonasreger@gmail.com>
2024-01-22 16:52:08 -0600

    reseting next configs

--- next.config.js
+++ next.config.js
@@ -26,7 +26,37 @@
 
 // export default withMDX(nextConfig)
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  /**
+   * enable static exports for the app router.
+   *
+   * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * set base path. this is usually the slug of your repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "wjonasreger.github.io",
+
+  /**
+   * disable server-based image optimization. next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
