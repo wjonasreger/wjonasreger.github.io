ecc56f687878855e30ac84c382125884b67bfcb2
wjonasreger <wjonasreger@gmail.com>
2024-01-22 22:10:32 -0600

    setting base path to /

--- next.config.js
+++ next.config.js
@@ -46,7 +46,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "wjonasreger.github.io",
+  basePath: "/",
 
   /**
    * disable server-based image optimization. next.js does not support
