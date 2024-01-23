0147781df01f99acfc35e65927d4afb5dbb278a5
wjonasreger <wjonasreger@gmail.com>
2024-01-22 22:30:04 -0600

    update intro text

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -31,8 +31,8 @@ export default function Home({ articles }) {
         url: SiteConfigs.siteUrl,
         images: [
           {
-            url: `./public/images/pfp-avatar.jpeg`,
-            width: 1200,
+            url: `../images/pfp-avatar.jpeg`,
+            width: 600,
             height: 600,
             alt: 'Og Image Alt',
             type: 'image/jpeg',
@@ -47,13 +47,13 @@ export default function Home({ articles }) {
             data science educator, language enthusiast, <br></br>context puzzler.
           </h1>
           <p className = "mt-6 prose dark:prose-invert">
-            👋 hello, i&apos;m jonas! i create language solutions with data and ai at puzzle labs. i&apos;m passionate about natural language processing, data visualization, and teaching others about data science.
+            👋 hello, i&apos;m jonas! i&apos;m a recent alum of uiuc statistics, specializing in data science. i&apos;m passionate about natural language processing (nlp), machine learning (ml), teaching others about data science, and making a positive and constructive impact on others. my primary research interest is using nlp towards language acquistion.
           </p>
           <p className = "mt-6 prose dark:prose-invert">
-            i&apos;ve worked in data science teaching roles at the university of illinois for equality, diversity, and inclusion. i&apos;ve also developed insights for linguistics in social media for r&d at sandia national labs.
+            i&apos;ve worked in data science teaching roles at the university of illinois for equality, diversity, and inclusion. i&apos;ve also developed insights for linguistics in social media at sandia national labs, and created knwoledge solutions for ai at puzzle labs.
           </p>
           <p className = "mt-6 prose dark:prose-invert">
-            feel free to have a looksie at my work here or on my github. i&apos;m always open to new opportunities, so feel free to reach out! also, if you&apos;re wondering why all lower caps, check out this <Link href = {`/articles/why-lower-case`}>post</Link> to get the scoop.
+            feel free to have a looksie at my work here or on my github. i&apos;m always open to new opportunities, so feel free to reach out! also, if you&apos;re wondering why all lower case, check out this <Link href = {`/articles/why-lower-case`}>post</Link> to get the scoop.
           </p>
           <div className = "mt-6 flex gap-6">
 
@@ -62,6 +62,7 @@ export default function Home({ articles }) {
               aria-label = "follow on github"
               icon = {GitHubIcon}
             />
+
           </div>
         </div>
       </Container>
