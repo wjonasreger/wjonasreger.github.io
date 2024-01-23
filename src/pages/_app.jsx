5531d9847a4ee28ce4e9fb48dd11a8dc440aa9cc
wjonasreger <wjonasreger@gmail.com>
2024-01-22 22:41:09 -0600

    remove layout from app

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -23,8 +23,7 @@ export default function App({ Component, pageProps, router }) {
 
     return (
      <>
-        <MDXProvider components={components}>
-            <Layout>
+        <MDXProvider components={components}>            
                 <div className="fixed inset-0 flex justify-center sm:px-8">
                     <div className="flex w-full max-w-7xl lg:px-8">
                         <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
@@ -36,7 +35,6 @@ export default function App({ Component, pageProps, router }) {
                         <Component previousPathname={prevPath} {...pageProps} />
                     </main>
                 </div>
-            </Layout>
         </MDXProvider>
      </>
     )
